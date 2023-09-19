3090f4910293e6bd3a783826b2b68ef983c1cef2
kurdsec1 <144842542+kurdsec1@users.noreply.github.com>
2023-09-19 23:47:49 +0300

    Delete m.py

--- m.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Kullanıcıdan isim al
-isim = input("Adınızı girin: ")
-
-# Merhaba mesajını oluştur
-mesaj = "Merhaba, " + isim + "! Bugün nasılsınız?"
-
-# Mesajı ekrana yazdır
-print(mesaj)
